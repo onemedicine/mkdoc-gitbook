c54a45a36e43874998a76917ee91ae2fc507a3ac
Lucas Ramage <ramage.lucas@protonmail.com>
2018-12-22 01:44:27 +0000

    initial commit

---
+++ mkdocs_gitbook/js/main.js
@@ -0,0 +1,16 @@
+var gitbook = gitbook || [];
+gitbook.push(function() {
+    gitbook.page.hasChanged({
+        "page": {},
+        "config": {},
+        "file": {},
+        "gitbook": {
+            "version": "3.2.3",
+            "time": "2018-06-16T01:44:55.224Z"
+        },
+        "basePath": ".",
+        "book": {
+            "language": ""
+        }
+    });
+});
