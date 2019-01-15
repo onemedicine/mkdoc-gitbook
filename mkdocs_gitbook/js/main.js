388db614d650dc586fa02c4d16b808f7b5dd3db1
Lucas Ramage <ramage.lucas@protonmail.com>
2019-01-15 21:02:51 +0000

    Prepend License for JavaScript
    Bugs: https://gitlab.com/lramage94/mkdocs-gitbook-theme/issues/3

--- mkdocs_gitbook/js/main.js
+++ mkdocs_gitbook/js/main.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @licstart magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt APACHE-2.0 @licend
+ * The above is the entire license notice for the JavaScript code in this page.
+ *
+ */
+ 
 var gitbook = gitbook || [];
 gitbook.push(function() {
     gitbook.page.hasChanged({
